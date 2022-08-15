949d69c304cd1c7dcabfeb66d2d45503408f1a11
emilyymoraless <memily212@gmail.com>
2022-08-15 11:24:32 -0700

    daily challenge completed

--- JS/script.js
+++ JS/script.js
@@ -6,3 +6,10 @@ console.log("Emily Morales")
 
 //Inserted favorite food 
 console.log("I love birria tacos")
+
+//Day-2 Daily Challenge
+console.log("Supercalifragilisticexpialidocious".length) 
+
+console.log(typeof 25)
+
+console.log(typeof 11011998n)
